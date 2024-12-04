6bc9c541587509d498850b6750a74e12c9370733
luongdmttek <luong.ngo@dmttek.com>
2024-12-04 14:43:55 +0700

    vi dist

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-const { createConfig } = require('@openedx/frontend-build');
-
-module.exports = createConfig('jest', {
-  setupFilesAfterEnv: [
-    '<rootDir>/src/setupTest.js',
-  ],
-  testTimeout: 20000,
-});
