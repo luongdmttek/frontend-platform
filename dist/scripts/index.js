ca78b806157c020bccf729b9b45c88033fdbbe9a
luongdmttek <luong.ngo@dmttek.com>
2025-12-24 18:56:10 +0700

    lang pack

---
+++ dist/scripts/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+export { default as GoogleAnalyticsLoader } from './GoogleAnalyticsLoader';
+export { default as LanguageLoader } from './LanguageLoader';
+//# sourceMappingURL=index.js.map
--- src/react/hooks/paragon/index.js
+++
@@ -1,2 +0,0 @@
-export { default as useTrackColorSchemeChoice } from './useTrackColorSchemeChoice';
-export { default as useParagonTheme } from './useParagonTheme';
